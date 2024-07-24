b3661b0115260fd30fdecb3f6bc5dbd1991b10cf
gitterarka <94968079+gitterarka@users.noreply.github.com>
2024-07-24 17:31:51 +0530

    Add files via upload

---
+++ 01_array.js
@@ -0,0 +1,22 @@
+
+const arr=[0,1,2,3,4,5,6]
+const arr1=["arka","debduutta"]
+//console.log(arr1[1])
+//ARRAY METHODS
+//arr.push(6)
+// arr1.pop()
+// arr1.pop()
+// console.log(arr1.pop())
+// console.log(arr1.pop())
+// console.log(arr1)
+//const a=arr.join()
+//console.log(arr);
+//console.log(a);
+
+//console.log("a" ,arr)
+//const myn1=arr.slice(1,3)// more like substring in java
+//console.log(myn1)
+// console.log("b", arr)
+// const myn2=arr.splice(1,3)// it manipulate the original array i.e,numbers gets deleted
+// console.log("c",arr)
+// console.log(myn2)
